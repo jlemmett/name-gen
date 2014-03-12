a2375b9766dfabb162d4667b3ff17e243ff42de1
Janne <jlemmetti@gmail.com>
2014-03-12 21:34:03 +0200

    Version number bump

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject name-gen "0.1.0-SNAPSHOT"
-  :description "version name generator 0.1.0 'raunchy tiger'"
+(defproject name-gen "0.2.0-SNAPSHOT"
+  :description "version name generator 0.2.0 'ahdasmielinen lammas'"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [enlive "1.1.5"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler name-gen.handler/app
          :init name-gen.handler/init
