758d305df08f946211b89e57f4390d75a121eebe
Janne <jlemmetti@gmail.com>
2014-03-18 22:02:32 +0200

    More scraping utils, data now includes most(?) Finnish adjectives (probably contains some in unfitting grammatical cases despite manual cleanup)

--- src/name_gen/util/util.clj
+++ src/name_gen/util/util.clj
@@ -17,3 +17,32 @@
      (vec (filter filter-pred (fetch-to-list url selector)))))
 
 
+(defn gen-numbers [start end step]
+  (take-while #(<= % end) (iterate #(+ % step) start)))
+
+(defn gen-sanakirja-urls []
+  (let [baseUrl "http://www.sanakirja.org/cat.php?id=4&l=17&start="
+        startParameters (gen-numbers 100 8200 100)]
+
+    (map #(str baseUrl %) startParameters)))
+
+(defn get-sanakirjaorg-words [url]
+  (fetch-to-list url [:.category :li]))
+
+(defn get-sanakirjaorg-words-test [url]
+  '(url url))
+
+(defn get-all-sanakirjaorg-words [urls]
+
+  (reduce (fn [allWords url] (concat allWords (get-sanakirjaorg-words url))) [] urls))
+
+;; (def all-sanakirja-adjectives (get-all-sanakirjaorg-words (gen-sanakirja-urls)))
+
+;; http://www.sanakirja.org/cat.php?id=4&l=17&start=100
+;; http://www.sanakirja.org/cat.php?id=4&l=17&start=200
+;; ..
+;; http://www.sanakirja.org/cat.php?id=4&l=17&start=8200
+
+;; TODO: Fetch all of the above programatically
+
+
